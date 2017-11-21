7db1b724589216415546eeca04dc5c757c6abdb8
Qiyang Chen <qiyang.chen@accenture.com>
2017-11-21 10:21:14 +0000

    tried buckets

--- Dataflow.py
+++ Dataflow.py
@@ -2,40 +2,42 @@
 import apache_beam as beam
 import re
 import sys
+from google.coud import bigquery
 
-def my_grep(line, term):
-    if re.match( r'^' + re.escape(term), line):
-        yield line
+# def my_grep(line, term):
+#     if re.match( r'^' + re.escape(term), line):
+#         yield line
 
-# PROJECT='kf'
-# BUCKET='coach'
+PROJECT='streamingTest1'
+BUCKET='streamingtest1'
+DATASET='test1'
 
-# def run():
-#    argv = [
-#       '--project={0}'.format(kf),
-#       '--job_name=examplejob2',
-#       '--save_main_session',
-#       '--staging_location=gs://{0}/staging/'.format(coach),
-#       '--temp_location=gs://{0}/staging/'.format(coach),
-#       '--runner=DataflowRunner'
-#    ]
+def run():
+   argv = [
+      '--project={0}'.format(PROJECT),
+      '--job_name=examplejob2',
+      '--save_main_session',
+      '--staging_location=gs://{0}/staging/'.format(BUCKET),
+      '--temp_location=gs://{0}/staging/'.format(BUCKET),
+      '--runner=DataflowRunner'
+   ]
         
 
 if __name__ == '__main__':
-    p = beam.Pipeline(argv=sys.argv);
-    input_1 = 'C:\\Users\\qiyang.chen\\Documents\\SpiDIY\\GCP\\Streaming\\Bookmark.json'
-    output = 'tmp/output.json'
-    searchTerm = 'IN_PROGRESS'
+    p = beam.Pipeline(argv=sys.argv)
+    input = 'gs://.format(BUCKET)/Bookmark.json'
+    output = bigquery.Dataset('format(PROJECT):format(DATASET)')
+    # searchTerm = 'IN_PROGRESS'
     (p
-        | 'Getfile' >> beam.io.ReadFromText(input_1) #input file
+        | 'Getfile' >> beam.io.ReadFromText(input) #input file
         # | beam. FlatMap (lambda line: count_number(line)) # perform processing
         # | beam. Bucket.by(SlidingWindows.of(24, HOURS)) #streaming window
         # | beam. ParDo.of(new Filter1())
         # | beam. newGroup1()
         # | beam. Pardo.of(new Filter2())
         # | beam. newTransform1()
-        | 'Grep' >> beam.FlatMap(lambda line: my_grep(line, searchTerm) )
-        | 'Write' >> beam.io.WriteToText(output) # write output
+        # | 'Grep' >> beam.FlatMap(lambda line: my_grep(line, searchTerm) )
+        | 'Write' >> beam.io.WritetoText((output) # write output
     )
     # lines = p | ...
     # sizes = lines | 'Length' >> beam.Map(lambda line: len(line))
