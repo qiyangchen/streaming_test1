84ee5fc9181f7629496a51583e9640f038239159
Qiyang Chen <qiyang.chen@accenture.com>
2017-11-21 11:24:10 +0000

    changes

--- Dataflow.py
+++ Dataflow.py
@@ -2,43 +2,44 @@
 import apache_beam as beam
 import re
 import sys
-from google.coud import bigquery
+from google.cloud import bigquery
 
 # def my_grep(line, term):
 #     if re.match( r'^' + re.escape(term), line):
 #         yield line
 
-PROJECT='streamingTest1'
+PROJECT='streamingtest1-186611'
 BUCKET='streamingtest1'
 DATASET='test1'
 
 def run():
-   argv = [
+    argv = [
       '--project={0}'.format(PROJECT),
       '--job_name=examplejob2',
       '--save_main_session',
       '--staging_location=gs://{0}/staging/'.format(BUCKET),
       '--temp_location=gs://{0}/staging/'.format(BUCKET),
       '--runner=DataflowRunner'
-   ]
-        
-
-if __name__ == '__main__':
-    p = beam.Pipeline(argv=sys.argv)
-    input = 'gs://.format(BUCKET)/Bookmark.json'
-    output = bigquery.Dataset('format(PROJECT):format(DATASET)')
+    ]   
+    p = beam.Pipeline(argv=argv)
+    input = 'gs://{0}/Bookmark.json'.format(BUCKET)
+    # dataset= '{0}:{1}'.format(PROJECT) .format(DATASET)
+    # output = bigquery.Dataset(dataset)
     # searchTerm = 'IN_PROGRESS'
     (p
         | 'Getfile' >> beam.io.ReadFromText(input) #input file
-        # | beam. FlatMap (lambda line: count_number(line)) # perform processing
-        # | beam. Bucket.by(SlidingWindows.of(24, HOURS)) #streaming window
-        # | beam. ParDo.of(new Filter1())
-        # | beam. newGroup1()
-        # | beam. Pardo.of(new Filter2())
-        # | beam. newTransform1()
-        # | 'Grep' >> beam.FlatMap(lambda line: my_grep(line, searchTerm) )
-        | 'Write' >> beam.io.WritetoText((output) # write output
+        # # | beam. FlatMap (lambda line: count_number(line)) # perform processing
+        # # | beam. Bucket.by(SlidingWindows.of(24, HOURS)) #streaming window
+        # # | beam. ParDo.of(new Filter1())
+        # # | beam. newGroup1()
+        # # | beam. Pardo.of(new Filter2())
+        # # | beam. newTransform1()
+        # # | 'Grep' >> beam.FlatMap(lambda line: my_grep(line, searchTerm) )
+        # | 'Write' >> beam.io.WritetoText(output) # write output
     )
     # lines = p | ...
     # sizes = lines | 'Length' >> beam.Map(lambda line: len(line))
-    p.run() # run the pipeline
+    p.run()
+
+if __name__ == '__main__':    
+    run() # run the pipeline
--- install_packages.sh
+++ install_packages.sh
@@ -2,5 +2,6 @@
 
 apt-get install python-pip
 pip install google-cloud-dataflow oauth2client==3.0.0 
+pip install --upgrade google-cloud-bigquery
 pip install --force six==1.10  # downgrade as 1.11 breaks apitools
 pip install -U pip
